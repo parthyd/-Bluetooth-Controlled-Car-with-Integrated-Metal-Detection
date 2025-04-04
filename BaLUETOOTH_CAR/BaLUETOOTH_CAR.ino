1c5e6f2ff0f23930caa1ff38941e654351e3cb64
parthyd <parthyad868@gmail.com>
2025-04-04 10:01:24 +0530

    Add files via upload
    Arduino file

---
+++ BaLUETOOTH_CAR/BaLUETOOTH_CAR.ino
@@ -0,0 +1,93 @@
+#include <AFMotor.h>
+
+// Motor shield setup
+AF_DCMotor motor1(1, MOTOR12_1KHZ); // Motor connected to M1 and M2
+AF_DCMotor motor2(2, MOTOR12_1KHZ); 
+AF_DCMotor motor3(3, MOTOR34_1KHZ); // Motor connected to M3 and M4
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+
+char command; // Variable to store incoming Bluetooth commands
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communication
+}
+
+void loop() {
+  if (Serial.available() > 0) { // Check if data is available to read
+    command = Serial.read(); // Read the incoming byte
+    
+    // Execute the corresponding command
+    switch (command) {
+      case 'F': // Forward
+        forward();
+        break;
+      case 'B': // Backward
+        backward();
+        break;
+      case 'L': // Left
+        left();
+        break;
+      case 'R': // Right
+        right();
+        break;
+      case 'S': // Stop
+        stop();
+        break;
+    }
+  }
+}
+
+void forward() {
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+}
+
+void backward() {
+  motor1.setSpeed(255);
+  motor1.run(BACKWARD);
+  motor2.setSpeed(255);
+  motor2.run(BACKWARD);
+  motor3.setSpeed(255);
+  motor3.run(BACKWARD);
+  motor4.setSpeed(255);
+  motor4.run(BACKWARD);
+}
+
+void left() {
+  motor1.setSpeed(200);
+  motor1.run(BACKWARD);
+  motor2.setSpeed(200);
+  motor2.run(BACKWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+}
+
+void right() {
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(200);
+  motor3.run(BACKWARD);
+  motor4.setSpeed(200);
+  motor4.run(BACKWARD);
+}
+
+void stop() {
+  motor1.setSpeed(0);
+  motor1.run(RELEASE);
+  motor2.setSpeed(0);
+  motor2.run(RELEASE);
+  motor3.setSpeed(0);
+  motor3.run(RELEASE);
+  motor4.setSpeed(0);
+  motor4.run(RELEASE);
+}
